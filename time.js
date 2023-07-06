c2a10b2ed37520a9cc6d095982b8f2a2bd03685b
VISHNUKOMMIREDDY <109330287+VISHNUKOMMIREDDY@users.noreply.github.com>
2023-07-07 00:01:41 +0530

    Add files via upload

---
+++ time.js
@@ -0,0 +1,18 @@
+if(new Date().getHours()>=0  && new Date().getHours()<12){
+    document.getElementById("demo").innerHTML="Good Morning!!! "+" Dear Customer Welcome To 'BARISTA' Thankyou For Visiting us. Please Place Your Order, Have a Good Day, Keep Smiling Always.";
+}
+else if(new Date().getHours()>=12 && new Date().getHours()<16){
+     document.getElementById("demo").innerHTML="Good Afternoon!!! "+" Dear Customer Welcome To 'BARISTA' Thankyou For Visiting us. Please Place Your Order, Have a Good Day, Keep Smiling Always.";
+}
+ else if(new Date().getHours()>=16 && new Date().getHours()<19){
+     document.getElementById("demo").innerHTML="Good Evening!!! "+" Dear Customer Welcome To 'BARISTA' Thankyou For Visiting us. Please Place Your Order, Have a Good Day, Keep Smiling Always.";
+}
+else{
+     document.getElementById("demo").innerHTML="Good Night!!! "+" Dear Customer Sorry...! Orders are Closed Now....!!!";
+}
+
+
+
+
+
+
